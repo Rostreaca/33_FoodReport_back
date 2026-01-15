19266a5d175a37cf40e74968ce25471fb6118d99
Baek Jun Geol <ajungeul93@gamil.com>
2026-01-15 16:41:43 +0900

    docs: 토큰 xml 파일 수정 - token xml 파일 작성

--- src/main/java/com/kh/foodreport/domain/token/model/dao/TokenMapper.java
+++ src/main/java/com/kh/foodreport/domain/token/model/dao/TokenMapper.java
@@ -8,7 +8,6 @@ import com.kh.foodreport.domain.token.model.vo.RefreshToken;
 @Mapper
 public interface TokenMapper {
 	
-	@Insert("INSERT INTO FR_TOKEN VALUES(SEQ_TOKEN_NO.NEXTVAL, #{token}, #{expiration}, #{memberNo})")
 	int saveToken(RefreshToken token);
 
 }
