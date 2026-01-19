51d5b17674fcb01b0834f6fe4f0de2492c21fd75
dltbxkfm86-creator <dltbxkfm86@gmial.com>
2026-01-19 12:17:44 +0900

    feat:제목리뷰조회완성 refactor:제목및전체조회 정렬수정 refactor:공지사항 offset수정

--- src/main/java/com/kh/foodreport/domain/admin/review/controller/AdminReviewController.java
+++ src/main/java/com/kh/foodreport/domain/admin/review/controller/AdminReviewController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kh.foodreport.domain.admin.review.model.dto.AdminReviewDTO;
 import com.kh.foodreport.domain.admin.review.model.dto.AdminReviewResponse;
 import com.kh.foodreport.domain.admin.review.model.service.AdminReviewService;
 import com.kh.foodreport.global.common.ApiResponse;
@@ -22,11 +21,20 @@ public class AdminReviewController {
 	
 	private final AdminReviewService reviewService;
 	
-	@GetMapping
+	@GetMapping // 전체 리뷰 목록 조회
 	public ResponseEntity<ApiResponse<AdminReviewResponse>> findAllReviews(@RequestParam(name="page", defaultValue ="1") int page) {
 		
 		AdminReviewResponse reviews = reviewService.findAllReviews(page);
 		
-		return ApiResponse.ok(reviews, "리뷰 조회 성공!");
+		return ApiResponse.ok(reviews, "리뷰 전체 조회 성공!");
+	}
+	
+	@GetMapping("/keyword")
+	public ResponseEntity<ApiResponse<AdminReviewResponse>> findByReviewTitle(@RequestParam(name="page", defaultValue ="1") int page
+																			,@RequestParam(name="reviewTitle")String reviewTitle) {
+		
+		AdminReviewResponse reviews = reviewService.findByReviewTitle(page, reviewTitle);
+		
+		return ApiResponse.ok(reviews, "리뷰 제목 조회 성공!");
 	}
 }
--- src/main/java/com/kh/foodreport/domain/admin/review/model/service/AdminReviewServiceImpl.java
+++ src/main/java/com/kh/foodreport/domain/admin/review/model/service/AdminReviewServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.kh.foodreport.domain.admin.review.model.dao.AdminReviewMapper;
 import com.kh.foodreport.domain.admin.review.model.dto.AdminReviewDTO;
 import com.kh.foodreport.domain.admin.review.model.dto.AdminReviewResponse;
+import com.kh.foodreport.global.exception.InvalidKeywordException;
 import com.kh.foodreport.global.util.PageInfo;
 import com.kh.foodreport.global.util.Pagenation;
 import com.kh.foodreport.global.validator.GlobalValidator;
@@ -34,6 +35,35 @@ public class AdminReviewServiceImpl implements AdminReviewService {
 		
 		List<AdminReviewDTO> reviews = reviewMapper.findAllReviews(pages);
 		
+		return createResponse(reviews ,pages);
+	}
+
+	@Override
+	public AdminReviewResponse findByReviewTitle(int page, String reviewTitle) {
+		
+		GlobalValidator.validateNo(page, "page번호가 0보단 작을순 없습니다.");
+		
+		if(reviewTitle == null || "".equals(reviewTitle.trim())) {
+			throw new InvalidKeywordException("키워드를 입력해주세요.");
+		}
+		
+		// 부분 개수 조회
+		int listCount = reviewMapper.countByReviewTitle(reviewTitle);
+		
+		log.info("부분개수조회{}", listCount);
+		
+		Map<String, Object> pages = pagenation.getPageRequest(listCount, page, 10);
+		
+		pages.put("reviewTitle", reviewTitle);
+		
+		List<AdminReviewDTO> reviews = reviewMapper.findByReviewTitle(pages);
+		
+		return createResponse(reviews, pages);
+	}
+	
+	// 중복 메소드 제거(응답 데이터 만드는 메소드)
+	private AdminReviewResponse createResponse(List<AdminReviewDTO> reviews
+											  ,Map<String, Object> pages) {
 		AdminReviewResponse response = new AdminReviewResponse();
 		
 		response.setAdminReviews(reviews);
