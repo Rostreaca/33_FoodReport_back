82b8062085ace035e04813073ba1b38a532a44b3
dltbxkfm86-creator <dltbxkfm86@gmial.com>
2026-01-29 11:51:44 +0900

    feat:Region VO클래스 생성 feat: RegionDTO 클래스 생성

---
+++ src/main/java/com/kh/foodreport/global/region/model/dto/RegionDTO.java
@@ -0,0 +1,21 @@
+package com.kh.foodreport.global.region.model.dto;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RegionDTO {
+	
+	private Long regionNo;
+	private String regionName;
+	
+}
